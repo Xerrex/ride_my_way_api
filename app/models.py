c2e03cfa92527931aad77faab1ab1775f063c14e
Alex Kagai <alexkagai@outlook.com>
2018-06-26 00:47:47 +0300

    [feature #158538465] Add Ride Model

--- app/models.py
+++ app/models.py
@@ -9,4 +9,20 @@ class User:
         self.name = name
         self.email = email
         self.password = generate_password_hash(password)
+
+
+class Ride:
+    """Define the Ride Model
+    """
+    def __init__(self,**kwargs):
+        """Create a Ride Instance
+        """
+
+        self.starting_point = kwargs['starting_point']
+        self.destination = kwargs['destination']
+        self.depart_time = kwargs['depart_time']
+        self.eta = kwargs['eta']
+        self.seats = kwargs['seats']
+        self.vehicle = kwargs['vehicle']
+        self.driver = kwargs['driver']
     
