dc5245985ccf99bb407c62d15d4c01f64d6941be
Alex Kagai <alexkagai@outlook.com>
2018-06-25 15:54:35 +0300

    [feature #158524259] Add User Model

---
+++ app/models.py
@@ -0,0 +1,13 @@
+"""Defines Data Models for the aplication"""
+from werkzeug.security import generate_password_hash
+
+
+
+class User:
+    """Defines the User Data Model"""
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = generate_password_hash(password)
+    
