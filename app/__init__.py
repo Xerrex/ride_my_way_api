96f9da8e174572aa3a6253fa1c72fadf877747e4
Alex Kagai <alexkagai@outlook.com>
2018-06-26 00:50:59 +0300

    [feature #158538465] Register the ride blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,7 @@ def create_app(env_config="config"):
     from .auth_bp import auth_BP as Authentication_Blueprint
     app.register_blueprint(Authentication_Blueprint)
 
+    from .ride_bp import ride_BP as Ride_Blueprint
+    app.register_blueprint(Ride_Blueprint)
+
     return app
