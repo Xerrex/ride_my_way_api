a6832860f7526764ad055e3494813a657d36b341
Alex Kagai <alexkagai@outlook.com>
2018-06-26 09:19:05 +0300

    [feature #158537803] Add Endpoint to get specific ride

--- app/ride_bp/__init__.py
+++ app/ride_bp/__init__.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .views import RidesResource
+from .views import RidesResource, RideResource
 
 ride_BP =  Blueprint('ride', __name__, url_prefix='/api/v1')
 
 ride_api = Api(ride_BP)
 ride_api.add_resource(RidesResource, '/rides', endpoint="rides")
+ride_api.add_resource(RideResource, '/rides/<rideId>', endpoint="ride")
