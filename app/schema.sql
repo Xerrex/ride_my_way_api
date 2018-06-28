3fd44147035ee6f741020266cd3a3d58546d33bc
Alex Kagai <alexkagai@outlook.com>
2018-06-28 17:41:39 +0300

    [feature #158670748] Setup database configs

---
+++ app/schema.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS rides;
+DROP TABLE IF EXISTS requests;
+
+CREATE TABLE users(
+    id uuid PRIMARY KEY,
+    name varchar(120) NOT NULL,
+    email varchar(120) NOT NULL UNIQUE,
+    password varchar(256) NOT NULL
+);
+
+CREATE TABLE rides(
+    id serial PRIMARY KEY,
+    starting_point varchar(256) NOT NULL,
+    destination varchar(256) NOT NULL,
+    depart_time varchar(256) NOT NULL,
+    eta varchar(256) NOT NULL,
+    seats int NOT NULL,
+    vehicle varchar(120) NOT NULL,
+    driver uuid
+);
+
+CREATE TABLE requests(
+    id serial PRIMARY KEY,
+    ride_id int NOT NULL,
+    user_id uuid NOT NULL,
+    destination varchar(256) NOT NULL
+);
