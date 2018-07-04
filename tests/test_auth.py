73e45da929e304de4412e176f6616e57a0b28fe2
Alex Kagai <alexkagai@outlook.com>
2018-07-04 08:49:17 +0300

    [feature #158754393] integrate db to auth blueprint tests
    The following commit also removes code from tests.__init__.py.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,12 +1,10 @@
 from unittest import main
 import json
 
-from flask import url_for
 
 from app import create_app
-from app.data.user_data import USERS
-
-from . import TestBase
+from app.db import initialize, get_db, close_db
+from basetest import TestBase
 
 
 class AuthCase(TestBase):
@@ -28,44 +26,71 @@ class AuthCase(TestBase):
         self.app = create_app("test")
         self.client = self.app.test_client()
 
+        with self.app.test_request_context():
+            close_db()
+            initialize() # create all tables
 
     def tearDown(self):
+        
+        with self.app.test_request_context():
+            close_db()
+            db = get_db()
+            # Drop all the tables
+            cursor = db.cursor()
+            cursor.execute("DROP TABLE users")
+            cursor.execute("DROP TABLE rides")
+            cursor.execute("DROP TABLE requests")
+            db.commit()
+            cursor.close()
+            db.close()
+            
         self.client.post('/api/v1/auth/logout', content_type='application/json')
-        USERS.clear()
-
         self.app = None
         self.client = None
 
-    def test_user_registration(self):
-        """Test user registration
-
-        Assert that a valid POST request to /api/v1/auth/register
-        registers a new user
+    def register(self):
+        """Create new users
         """
 
         response = self.client.post('/api/v1/auth/register', 
                                     data=json.dumps(self.test_user), 
                                     content_type='application/json')
+        return response
+    
+    def login(self):
+        """Starts a user session
+        """
+        response = self.client.post('/api/v1/auth/login', 
+                                    data=json.dumps(self.test_login), 
+                                    content_type='application/json')
+        return response
+
+
+    def test_user_registration(self):
+        """Test user can registration
+
+        Assert that a valid POST request to /api/v1/auth/register
+        registers a new user
+        """
+
+        response = self.register()
 
         self.assert201(response)
 
         response_data = json.loads(response.get_data(as_text=True))
         self.assertIn("User Account Was Created Successfully.", response_data['message'])
     
+
     def test_user_log_in(self):
-        """Test user login
+        """Test user can login
 
         Assert that a valid POST request to /api/v1/auth/login
         starts a User session:
         """
 
-        response = self.client.post('/api/v1/auth/register', 
-                                    data=json.dumps(self.test_user), 
-                                    content_type='application/json')
+        self.register()
                                     
-        response = self.client.post('/api/v1/auth/login', 
-                                    data=json.dumps(self.test_login), 
-                                    content_type='application/json')
+        response = self.login()
         
         self.assert200(response)
 
@@ -74,19 +99,15 @@ class AuthCase(TestBase):
         self.assertIn(self.test_user['name'], data['message'])
 
     def test_duplicate_user_registration(self):
-        """Test duplicate User Registration
+        """Test user cannot register twice
 
         Assert that a valid POST request to /api/v1/auth/register
         twice with same data fails with 409 status code
         """
 
-        response = self.client.post('/api/v1/auth/register', 
-                                    data=json.dumps(self.test_user), 
-                                    content_type='application/json')
+        self.register()
         
-        response = self.client.post('/api/v1/auth/register', 
-                                    data=json.dumps(self.test_user), 
-                                    content_type='application/json')
+        response = self.register()
         
         self.assert409(response)
 
@@ -95,23 +116,17 @@ class AuthCase(TestBase):
         self.assertIn(message, 'User with that email already exists.')
 
     def test_user_is_already_logged_in(self):
-        """Test User is already already logged in
+        """Test user cannot logged twice
 
         Assert that a valid Post request to /api/v1/auth/login
         when a user is already logged in fails with 409
         """
 
-        response = self.client.post('/api/v1/auth/register', 
-                                    data=json.dumps(self.test_user), 
-                                    content_type='application/json')
+        self.register()
 
-        response = self.client.post('/api/v1/auth/login',
-                                    data=json.dumps(self.test_login),
-                                    content_type='application/json')
+        self.login()
 
-        response = self.client.post('/api/v1/auth/login', 
-                                    data=json.dumps(self.test_login),
-                                    content_type='application/json')
+        response = self.login()
 
         self.assert409(response)
 
@@ -120,14 +135,12 @@ class AuthCase(TestBase):
         self.assertEqual("Make sure to logout first", message)
 
     def test_non_registered_user_login(self):
-        """Test login for a user that is not registered
+        """Test user cannot login if not registered
 
         Assert that a valid POST request to /api/v1/auth/login
         """
 
-        response = self.client.post('/api/v1/auth/login',
-                                    data=json.dumps(self.test_login),
-                                    content_type='application/json')
+        response = self.login()
         
         self.assert401(response)
 
@@ -136,19 +149,15 @@ class AuthCase(TestBase):
         self.assertIn(expected_msg, msg)
 
     def test_user_can_logout(self):
-        """Test logout
+        """Test user can logout
 
         Assert that a valid POST Request to /api/v1/auth/logout
         Ends the current user session
         """
 
-        response = self.client.post('/api/v1/auth/register', 
-                                    data=json.dumps(self.test_user), 
-                                    content_type='application/json')
+        self.register()
 
-        response = self.client.post('/api/v1/auth/login',
-                                    data=json.dumps(self.test_login),
-                                    content_type='application/json')
+        self.login()
 
         response = self.client.post('/api/v1/auth/logout',
                                     content_type='application/json')
@@ -159,7 +168,7 @@ class AuthCase(TestBase):
 
 
     def test_logout_for_logged_in_user(self):
-        """Test that only logged in user can logout
+        """Test user can logout only if logged in
 
         Assert that a POST Request to /api/v1/auth/logout
         fails if user is not logged in.
