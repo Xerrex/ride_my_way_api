4229d2d733e170d3271dbeaf5bf8749505774a2c
Alex Kagai <alexkagai@outlook.com>
2021-04-13 23:02:26 +0300

    config app to use Sqlite3

--- app/db.py
+++ app/db.py
@@ -1,4 +1,4 @@
-from psycopg2 import connect
+import sqlite3
 
 
 import click
@@ -11,18 +11,17 @@ def  get_db():
     returns a Database connection
     """
     if 'db' not in g:
-        g.db = connect(
-                host=current_app.config['DATABASE_HOST'],
-                dbname=current_app.config['DATABASE'], 
-                user=current_app.config['DATABASE_USER'], 
-                password=current_app.config['DATABASE_PASS']
+        g.db = sqlite3.connect(
+                current_app.config['DATABASE'],
+                detect_types=sqlite3.PARSE_DECLTYPES
             )
+        g.db.row_factory = sqlite3.Row
         return g.db
 
 
 def close_db(e=None):
-    """If this request connected to the database, close the
-    connection.
+    """If this request connected to the 
+        database, close the connection.
     """
     db = g.pop('db', None)
 
@@ -37,12 +36,20 @@ def initialize():
     """
     db = get_db()
 
-    with current_app.open_resource('schema.sql') as f, db.cursor() as cursor:
-        cursor.execute(f.read().decode('utf8'))       
-        db.commit()
-        cursor.close()  
-        db.close()   
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))   
         
+def seed_data():
+    """Insert data mock data into the database.
+    """
+    # insert rides
+    query = """INSERT INTO rides(starting_point, 
+        destination, depart_time, eta, 
+        seats, vehicle,driver) VALUES
+    """
+    # TODO: finish a seed function
+    pass
+
 
 @click.group()
 def db():
@@ -53,15 +60,17 @@ def db():
 @db.command()
 @with_appcontext
 def init():
-    """Create initial db tables"""
+    """Clear existing data & create 
+        initial db tables.
+    """
 
     initialize()
     click.echo('Initialized the database.')
 
 
 def init_app(app):
-    """Register database functions with the Flask app. This is called by
-    the application factory.
+    """Register database functions with the Flask 
+    app. This is called by the application factory.
     """
     app.teardown_appcontext(close_db)
     app.cli.add_command(db)
