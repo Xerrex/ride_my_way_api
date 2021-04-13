d4a63f278565e7437024ab9fc0c87162a6068886
Alex Kagai <alexkagai@outlook.com>
2021-04-13 23:05:52 +0300

    Refactor code to stop using app/__init__.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import os
 
-from app import create_app
+from app.core import create_app
 
-app = create_app()
+app = create_app(os.environ.get("ENV_CONFIG"))
 
 if __name__ == '__main__':
     app.run()
